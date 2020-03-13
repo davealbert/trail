cf77b5c0a72c6421856dafe7922f99cf30cf32de
Dave Albert <dave@one15digital.com>
2020-03-13 16:13:57 +0000

    lint fixes

--- packages/trail-core/example/index.js
+++ packages/trail-core/example/index.js
@@ -1,9 +1,9 @@
-const {TrailsManager} = require('@nearform/trail-core')
+const { TrailsManager } = require('@nearform/trail-core')
 
-const main = async function() {
+const main = async function () {
   const manager = new TrailsManager()
 
-  const id = await manager.insert({when: new Date(), who: 'user:1', what: 'open', subject: 'page:1'})
+  const id = await manager.insert({ when: new Date(), who: 'user:1', what: 'open', subject: 'page:1' })
   const trail = await manager.get(id)
   console.log(trail.who)
 
